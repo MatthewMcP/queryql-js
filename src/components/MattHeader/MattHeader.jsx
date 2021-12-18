712b3ebe2cb4bf1140f53b81025109ca0e39edfe
matthewmcp <matthew.mcparland@gmail.com>
2021-12-18 14:24:42 +0000

    feat: improving header transtion

--- src/components/MattHeader/MattHeader.jsx
+++ src/components/MattHeader/MattHeader.jsx
@@ -5,7 +5,7 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
 
-const MattHeader = () => {
+const MattHeader = ({ bgColour = blueGrey[500] }) => {
     const [mouseOver, setMouseOver] = useState(false);
 
     return (
@@ -17,34 +17,32 @@ const MattHeader = () => {
                 setMouseOver(false);
             }}
             sx={{
-                bgcolor: blueGrey[500],
+                bgcolor: bgColour,
                 borderRadius: '5px',
                 color: blueGrey[50],
                 textAlign: 'right',
                 verticalAlign: 'top',
                 float: 'right',
                 padding: '3px',
+                height: '23px',
+                overflow: 'hidden',
             }}>
-            <Collapse orientation='horizontal' in={mouseOver} collapsedSize={23} timeout={200}>
-                {mouseOver ? (
-                    <>
-                        <span pb='10px'>Made by Matthew McParland</span>
-                        {'    '}
-                        <a href='https://github.com/MatthewMcP' target='_blank' rel='noopener noreferrer'>
-                            <GitHubIcon sx={{ color: 'white' }} fontSize='small' />
-                        </a>
-                        {'    '}
-                        <a href='https://www.linkedin.com/in/mmcparland/' target='_blank' rel='noopener noreferrer'>
-                            <LinkedInIcon sx={{ color: 'white' }} fontSize='small' />
-                        </a>
-                        {'    '}
-                        <a href='mailto:matthew.mcparland@gmail.com' target='_blank' rel='noopener noreferrer'>
-                            <EmailIcon sx={{ color: 'white' }} fontSize='small' />
-                        </a>{' '}
-                    </>
-                ) : (
-                    <>&nbsp;&gt;&nbsp;</>
-                )}
+            <Collapse orientation='horizontal' in={mouseOver} collapsedSize={22} timeout={300}>
+                &gt;&nbsp;
+                <span>Made by Matthew McParland</span>
+                &nbsp;&nbsp;
+                <a href='https://github.com/MatthewMcP' target='_blank' rel='noopener noreferrer'>
+                    <GitHubIcon sx={{ color: 'white' }} fontSize='small' />
+                </a>
+                &nbsp;&nbsp;
+                <a href='https://www.linkedin.com/in/mmcparland/' target='_blank' rel='noopener noreferrer'>
+                    <LinkedInIcon sx={{ color: 'white' }} fontSize='small' />
+                </a>
+                &nbsp;&nbsp;
+                <a href='mailto:matthew.mcparland@gmail.com' target='_blank' rel='noopener noreferrer'>
+                    <EmailIcon sx={{ color: 'white' }} fontSize='small' />
+                </a>
+                &nbsp;
             </Collapse>
         </Box>
     );
